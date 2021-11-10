68e86b5194c726e55e38e9996c6f1c8fe3f6915e
Anderson Godoy <anderson.a.godoy@gmail.com>
2021-11-10 14:06:01 -0300

    Refatora codigo para aceitar Banco de dados com typeorm

--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,20 +1,21 @@
+import { inject, injectable } from 'tsyringe';
 import { ISpecificationRepository } from '../../repositories/ISpecificationsRepository';
 
 interface IRequest {
 	name: string;
 	description: string;
 }
-
+@injectable()
 class CreateSpecificationUseCase {
-	constructor(private specificationsRepository: ISpecificationRepository) {}
-	execute({ name, description }: IRequest): void {
-		const specificcationAlreadyExists = this.specificationsRepository.findByName(name);
+	constructor(@inject('SpecificationsRepository') private specificationsRepository: ISpecificationRepository) {}
+	async execute({ name, description }: IRequest): Promise<void> {
+		const specificcationAlreadyExists = await this.specificationsRepository.findByName(name);
 
 		if (specificcationAlreadyExists) {
 			throw new Error('Specification already exists!');
 		}
 
-		this.specificationsRepository.create({
+		await this.specificationsRepository.create({
 			name,
 			description
 		});
