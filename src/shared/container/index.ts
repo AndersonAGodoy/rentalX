c602a71b1740beccb72f1c426befe1c6e03ada69
Anderson Godoy <anderson.a.godoy@gmail.com>
2022-01-29 18:10:28 -0300

    feat: cria testes para os casos de uso

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -5,7 +5,10 @@ import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRep
 import { ISpecificationRepository } from '@modules/cars/repositories/ISpecificationsRepository';
 import { CategoriesRepository } from '@modules/cars/infra/typeorm/repositories/CategoriesRepository';
 import { SpecificationsRepository } from '@modules/cars/infra/typeorm/repositories/SpecificationRepository';
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository';
 
 container.registerSingleton<ICategoriesRepository>('CategoriesRepository', CategoriesRepository);
 container.registerSingleton<ISpecificationRepository>('SpecificationsRepository', SpecificationsRepository);
 container.registerSingleton<IUsersRepository>('UsersRepository', UsersRepository);
+container.registerSingleton<ICarsRepository>('CarsRepository', CarsRepository);
